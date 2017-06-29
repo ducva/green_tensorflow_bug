0f47acf674c48ba702c9ed9abb17db695cb1efa0
Nathan Stocks <nathan.stocks@gmail.com>
2017-06-29 11:12:09 -0600

    Fix testing under green by not instantiating complex objects during import.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 import unittest
 import numpy as np
-from global_service import predictor
+from service import PredictService
 
 
 class Test(unittest.TestCase):
@@ -15,7 +15,7 @@ class Test(unittest.TestCase):
             [0,0.5,0,0,0,0,0,0,2,0,0,0,0]
         ])
         print("Test data: %s" % str(test_data))
-        labels=predictor.predict(test_data)
+        labels = PredictService().predict(test_data)
         expected = [0]
         self.assertListEqual(labels, expected)
 
